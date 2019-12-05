67ccfd39aca10738bdf4c1638493bf4fea67fdbb
baoqingna <41460074+baoqingna@users.noreply.github.com>
2019-12-06 01:40:47 +0800

    Form create

--- resources/views/Blog.blade.php
+++ resources/views/Blog.blade.php
@@ -3,30 +3,35 @@
 @section('form')
 
 <div class="container">
-
-  {{-- {!! Form::open() !!}
-    <div class="card-header"> Tasks </div>
-    <div class="card-body">
-        {!! Form::label('name', 'Name') !!}
-        {!! Form::text('name', null, ['class' => 'form-control']) !!}
-    </div>
-    <div class="card-footer">
-        {!! Form::submit('Add Task', ['class' => 'btn btn-primary']) !!}
-    </div>
-  {!! Form::close() !!} --}}
-
-    <hr>
-
     <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
+
+            {!! Form::open() !!}
+            <div class="card-header">Create Post </div>
+            <div class="card-body">
+                {!! Form::label('title', 'Blog Title') !!}
+                {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                <br>
+                {!! Form::label('desc', 'Blog Description') !!}
+                {!! Form::text('desc', null, ['class' => 'form-control']) !!}
+            </div>
+            <div class="card-footer">
+                {!! Form::submit('Add Post', ['class' => 'btn btn-primary']) !!}
+            </div>
+            {!! Form::close() !!}
+
+            <hr>
+            <br>
+
+
             {{-- @foreach ($iterable as $key => $value) --}}
             <div class="post-preview">
                 <a href="/">
                     <h2 class="post-title"> Man must explore, and this is exploration at its greatest</h2>
-                  <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
+                    <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
                     <p class="post-meta">
-                      <a href="#">Edit</a>
-                      <a href="#">Delete</a>
+                        <a href="#">Edit</a>
+                        <a href="#">Delete</a>
                     </p>
                 </a>
             </div>
