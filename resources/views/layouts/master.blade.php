2cffc64226616f8f95db1959bf84c315e033f47b
baoqingna <41460074+baoqingna@users.noreply.github.com>
2019-12-07 19:39:05 +0800

    List

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -33,10 +33,10 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-md-10 mx-auto">
+                @yield('list')
+                <br>
                 @yield('form')
                 @yield('formEdit')
-                <br>
-                @yield('list')
             </div>
         </div>
     </div>
